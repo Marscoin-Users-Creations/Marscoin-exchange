0d622fd21bcca969f66d538fc525c6f744d480a0
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-03-13 18:50:15 +0100

    Created main.cpp

---
+++ main.cpp
@@ -0,0 +1,40 @@
+// default requires
+
+#include <std>
+//"remove all content between garments and the slashes before them if you compile the code for a Windows Marscoin Exchange server ( .exe app )"#include <Windows.h>
+//"remove all content between garments and the slashed before them if you compile the code for a Linux Marscoin Exchange server ( .elf app )"#include <unistd.h>
+
+#include "internet/internet.cpp"
+#include "wallets/wallets.cpp"
+#include "website/website.cpp"
+
+
+// default vars
+
+
+
+
+// main namespace
+
+namespace main() {
+    
+    function internet() {
+        
+        
+        
+    };
+    function wallets() {
+        
+        int bitcoinWallet = "";
+        int bitcoinCashWallet = "";
+        int litecoinWallet = "";
+        int marscoinWallet = "";
+        
+    };
+    function website() {
+        
+        
+        
+    };
+    
+};
