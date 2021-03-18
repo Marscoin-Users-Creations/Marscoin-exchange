0f594246996ebb1376586258125dccee3958943f
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-03-18 19:39:01 +0100

    Created coins.cpp

---
+++ coins/coins.cpp
@@ -0,0 +1,28 @@
+// default includes
+
+#include <std>
+
+#include "coins.json"
+#include "coins.hpp"
+#include "../internet/internet.cpp"
+
+
+// default vars
+
+int bitcoinDefaultWalletPort = 0;
+int dogecoinDefaultWalletPort = 0;
+int ethereumDefaultWalletPort = 0;
+int libertycoinDefaultWalletPort = 0;
+int litecoinDefaultWalletPort = 0;
+int marscoinDefaultWalletPort = 0;
+int megacoinDefaultWalletPort = 0;
+int spacecoinDefaultWalletPort = 0;
+
+
+// coins namespace
+
+namespace coins() {
+    
+    
+    
+};
